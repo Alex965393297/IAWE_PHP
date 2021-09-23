28a624654f18b96334ac3a08d0eed9e8bc79ef7b
Alejandro Garcia Hidalgo <alex@CliAlex.asir2.iespacomolla.es>
2021-09-23 15:54:43 +0200

    documento

---
+++ ejemplo1.php
@@ -0,0 +1,13 @@
+<?php
+    $peso = 43;
+    $altura = 1.70;
+    $imc = $peso/($altura*$altura);
+    echo "tu indice de masa corporal es : $imc ";
+
+    if ($imc >30 )
+    echo "pierde peso";
+
+    else [
+        echo "tu peso parece correcto";
+    ]
+
